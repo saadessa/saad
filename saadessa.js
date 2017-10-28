cd89014e228ee352ff1f0721c521b13f7c77eeff
saadessa <32783963+saadessa@users.noreply.github.com>
2017-10-29 01:46:34 +0200

    Update saadessa.js

--- saadessa.js
+++ saadessa.js
@@ -1,4 +1,3 @@
-
 function fillTable(result) {
                 var table = document.getElementById("inbound_keywords").tBodies[0];
                 var data = result.data;
@@ -8,10 +7,7 @@ function fillTable(result) {
             }
            
             var script = document.createElement("script");
-            script.src = "https://api.tynt.com/publisher/v1/keyword/inbound?site_guid=dsatR-UL8r5OkGrkHcnnVW&api_key=6M7C6U7F7g4N7P6J7H4x3o6D4j3L6s7P&hours=12&callback=foo.bar1";
+            script.src = "https://api.tynt.com/publisher/v1/keyword/inbound?site_guid=dsatR-UL8r5OkGrkHcnnVW&api_key=6M7C6U7F7g4N7P6J7H4x3o6D4j3L6s7P&hours=12&callback=fillTable";
             script.async = "async";
             document.getElementsByTagName("head")[0].appendChild(script);
 document.write(document.referrer);
-
-
-
