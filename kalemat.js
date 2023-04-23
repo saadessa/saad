58194945cb400ad4e7b8c72c7d435d46eb51ab2c
saadessa <32783963+saadessa@users.noreply.github.com>
2023-04-23 20:42:19 +0200

    Update kalemat.js

--- kalemat.js
+++ kalemat.js
@@ -43,5 +43,25 @@ f.add("استعلام", "https://arabiansoft.blogspot.com/2023/03/blog-post.html
 f.add("تونس", "https://arabiansoft.blogspot.com/2022/11/watch-tunisia-and-denmark-match.html");
 f.add("منظومة الجوازات", "https://arabiansoft.blogspot.com/2017/05/2017.html");
 f.add("بطاقات", "https://arabiansoft.blogspot.com/2023/03/10000.html");
+  f.add("علاج البواسير", "https://sahhatk.blogspot.com/2015/03/blog-post_9.html");
+f.add("علاج البواسير بالثوم", "https://sahhatk.blogspot.com/2015/03/blog-post_67.html");
+f.add("الثوم", "https://sahhatk.blogspot.com/2015/03/blog-post_89.html");
+f.add("القولون العصبي", "https://sahhatk.blogspot.com/2017/06/blog-post_68.html");
+f.add("السكري من النوع الثاني", "https://sahhatk.blogspot.com/2019/01/blog-post.html");
+f.add("نظام غذائي", "https://sahhatk.blogspot.com/2019/01/blog-post_7.html");
+f.add("فقدان الوزن", "https://sahhatk.blogspot.com/2019/01/blog-post_7.html");
+f.add("الإمساك", "https://sahhatk.blogspot.com/2017/05/Treatment-constipation.html");
+ f.add("ارتفاع ضغط الدم", "https://sahhatk.blogspot.com/2017/12/hypertension.html");
+ f.add("تساقط الشعر", "#");
+f.add("سرطان الثدي", "https://sahhatk.blogspot.com/2017/06/blog-post_18.html");
+ f.add("سرطان الرئة", "https://sahhatk.blogspot.com/2017/05/l.html");
+f.add("مرضى السكر", "https://sahhatk.blogspot.com/2015/03/blog-post_61.html");
+f.add("الحمل", "https://sahhatk.blogspot.com/2015/03/blog-post_88.html");
+f.add("الولادة", "https://sahhatk.blogspot.com/2015/03/blog-post_58.html");
+f.add("أمراض القلب", "https://sahhatk.blogspot.com/2015/03/blog-post_41.html");
+  f.add("التمارين الرياضية", "https://sahhatk.blogspot.com/2019/01/blog-post_7.html");
+  f.add("المكسرات", "https://sahhatk.blogspot.com/2015/03/blog-post_63.html");
+  f.add("البواسير الخارجية", "https://sahhatk.blogspot.com/2023/04/blog-post_19.html");
+
 
 f.startScript();
